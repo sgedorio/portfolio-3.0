df73c8167f4099de8e4e556cce5358479a892357
Sean Gedorio <sgedorio@gmail.com>
2024-03-06 21:37:16 -0600

    adding vercel's speed insights

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 // import { Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import About from './pages/About';
