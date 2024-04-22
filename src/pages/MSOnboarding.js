005670283c0f4865c8711c7a88a7906ad43aba6e
Sean Gedorio <sgedorio@gmail.com>
2024-04-22 11:54:06 -0600

    back to 600ms

--- src/pages/MSOnboarding.js
+++ src/pages/MSOnboarding.js
@@ -56,7 +56,7 @@ export default function MSOnboarding() {
         // Simulate loading delay
         const timeout = setTimeout(() => {
           setImgsLoaded(true);
-        }, 800);
+        }, 600);
     
         // Clear the timeout to avoid memory leaks
         return () => clearTimeout(timeout);
--- src/pages/SupplyMe.js
+++ src/pages/SupplyMe.js
@@ -41,7 +41,7 @@ export default function SupplyMe() {
         // Simulate loading delay
         const timeout = setTimeout(() => {
           setImgsLoaded(true);
-        }, 800);
+        }, 600);
     
         // Clear the timeout to avoid memory leaks
         return () => clearTimeout(timeout);
